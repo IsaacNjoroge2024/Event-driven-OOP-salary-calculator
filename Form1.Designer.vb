d96993a7dae3a89fc31066308ed72bcef5d7d91c
nyashlord <iwanjaflores@gmail.com>
2024-05-05 22:34:46 +0300

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,357 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.cmbBenefits = New System.Windows.Forms.ComboBox()
+        Me.txtBasic = New System.Windows.Forms.TextBox()
+        Me.cmbDesig = New System.Windows.Forms.ComboBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(-1, -2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(807, 81)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(13, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(604, 65)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Salary System For Google"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Gold
+        Me.GroupBox1.Controls.Add(Me.CheckBox6)
+        Me.GroupBox1.Controls.Add(Me.CheckBox5)
+        Me.GroupBox1.Controls.Add(Me.CheckBox4)
+        Me.GroupBox1.Controls.Add(Me.CheckBox3)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.cmbBenefits)
+        Me.GroupBox1.Controls.Add(Me.txtBasic)
+        Me.GroupBox1.Controls.Add(Me.cmbDesig)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 85)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(794, 369)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Employee Details"
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.AutoSize = True
+        Me.CheckBox6.ForeColor = System.Drawing.Color.Blue
+        Me.CheckBox6.Location = New System.Drawing.Point(512, 323)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(208, 29)
+        Me.CheckBox6.TabIndex = 14
+        Me.CheckBox6.Text = "Utility Allowance 12%"
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.ForeColor = System.Drawing.Color.Blue
+        Me.CheckBox5.Location = New System.Drawing.Point(512, 274)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(227, 29)
+        Me.CheckBox5.TabIndex = 13
+        Me.CheckBox5.Text = "Transport Allowance 9%"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.ForeColor = System.Drawing.Color.Blue
+        Me.CheckBox4.Location = New System.Drawing.Point(258, 323)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(150, 29)
+        Me.CheckBox4.TabIndex = 12
+        Me.CheckBox4.Text = "Other Benefits"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.ForeColor = System.Drawing.Color.Blue
+        Me.CheckBox3.Location = New System.Drawing.Point(258, 274)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(248, 29)
+        Me.CheckBox3.TabIndex = 11
+        Me.CheckBox3.Text = "Conveyance Allowance 8%"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.ForeColor = System.Drawing.Color.Blue
+        Me.CheckBox2.Location = New System.Drawing.Point(22, 323)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(196, 29)
+        Me.CheckBox2.TabIndex = 10
+        Me.CheckBox2.Text = "Medical Benefits 7%"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.ForeColor = System.Drawing.Color.Blue
+        Me.CheckBox1.Location = New System.Drawing.Point(22, 274)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(230, 29)
+        Me.CheckBox1.TabIndex = 9
+        Me.CheckBox1.Text = "Housing Allowance 35%"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'cmbBenefits
+        '
+        Me.cmbBenefits.FormattingEnabled = True
+        Me.cmbBenefits.Items.AddRange(New Object() {"New Year Bonus", "Diwali Bonus", "Weekend Bonus", "Overtime Bonus", "Holiday Bonus"})
+        Me.cmbBenefits.Location = New System.Drawing.Point(237, 213)
+        Me.cmbBenefits.Name = "cmbBenefits"
+        Me.cmbBenefits.Size = New System.Drawing.Size(220, 33)
+        Me.cmbBenefits.TabIndex = 8
+        '
+        'txtBasic
+        '
+        Me.txtBasic.Location = New System.Drawing.Point(237, 168)
+        Me.txtBasic.Name = "txtBasic"
+        Me.txtBasic.Size = New System.Drawing.Size(220, 31)
+        Me.txtBasic.TabIndex = 7
+        '
+        'cmbDesig
+        '
+        Me.cmbDesig.FormattingEnabled = True
+        Me.cmbDesig.Items.AddRange(New Object() {"Project Manager ", "IT officer", "Accountant", "Marketing Manager"})
+        Me.cmbDesig.Location = New System.Drawing.Point(237, 100)
+        Me.cmbDesig.Name = "cmbDesig"
+        Me.cmbDesig.Size = New System.Drawing.Size(220, 33)
+        Me.cmbDesig.TabIndex = 6
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(237, 50)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(220, 31)
+        Me.txtName.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(22, 216)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 25)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Benefits"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(22, 157)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(103, 25)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Basic Salary"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 103)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(190, 25)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Employee Designation"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(22, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(142, 25)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Employee Name"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label6.Location = New System.Drawing.Point(17, 466)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(93, 25)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Gross Pay:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label7.Location = New System.Drawing.Point(17, 510)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(76, 25)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Net Pay:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(116, 460)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(150, 31)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(114, 507)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(150, 31)
+        Me.TextBox4.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.ForeColor = System.Drawing.Color.Black
+        Me.Button1.Location = New System.Drawing.Point(361, 480)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(197, 82)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Calculate Salary"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label8.Location = New System.Drawing.Point(17, 555)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(72, 25)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "Tax Pay:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(114, 552)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(150, 31)
+        Me.TextBox1.TabIndex = 10
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(629, 505)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(112, 34)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(806, 589)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label6)
+        Me.Name = "Form1"
+        Me.Text = "Salary Calculator"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents cmbBenefits As ComboBox
+    Friend WithEvents txtBasic As TextBox
+    Friend WithEvents cmbDesig As ComboBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sal As Double
+        Dim hse As Double
+        Dim con As Double
+        Dim med As Double
+        Dim trans As Double
+        Dim util As Double
+        Dim bonus As Double
+        Dim gross As Double
+        Dim net As Double
+        Dim tax As Double
+
+        sal = Convert.ToDecimal(txtBasic.Text)
+        If CheckBox1.Checked = True Then
+            hse = (35 / 100) * sal
+        End If
+
+        If CheckBox3.Checked = True Then
+            con = (8 / 100) * sal
+        End If
+
+        If CheckBox5.Checked = True Then
+            trans = (9 / 100) * sal
+        End If
+
+        If CheckBox2.Checked = True Then
+            med = (7 / 100) * sal
+        End If
+
+        If CheckBox6.Checked = True Then
+            util = (12 / 100) * sal
+        End If
+
+        If CheckBox4.Checked = True Then
+            If cmbBenefits.Text = "New Year Bonus" Then
+                bonus = 3500
+            End If
+            If cmbBenefits.Text = "Diwali Bonus" Then
+                bonus = 5000
+            End If
+            If cmbBenefits.Text = "Weekend Bonus" Then
+                bonus = (1 / 100) * sal
+            End If
+            If cmbBenefits.Text = "Overtime Bonus" Then
+                bonus = 6500
+            End If
+
+        End If
+        gross = bonus + hse + con + med + trans + util + sal
+        tax = (15 / 100) * sal
+        net = gross - tax
+        TextBox3.Text = Convert.ToDouble(gross)
+        TextBox4.Text = Convert.ToDouble(net)
+        TextBox1.Text = Convert.ToDouble(tax)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        cmbBenefits.Text = ""
+        cmbDesig.Text = ""
+        txtBasic.Clear()
+        txtName.Clear()
+    End Sub
+End Class
